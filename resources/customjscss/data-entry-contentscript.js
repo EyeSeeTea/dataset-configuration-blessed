b8e25c1adc65fc2a118f5c7db0a3fce90fcb84bb
Arnau Sanchez <pyarnau@gmail.com>
2017-07-06 13:49:35 +0200

    Move custom css for webentry to css file

--- resources/customjscss/data-entry-contentscript.js
+++ resources/customjscss/data-entry-contentscript.js
@@ -12,140 +12,22 @@
         - Groups within a theme are grouped.
 */
 
+(function() {
+
 var runOnMutation = function(contentEl, callback) {
     var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
     if (!MutationObserver) throw "Mutation is not supported by this browser";
-    var hasChildren = contentEl.children().size() > 0;
+    var hasChildrenState = contentEl.children().size() > 0;
     var observer = new MutationObserver(function() {
-        if (!hasChildren && contentEl.children().size() > 0) {
+        var hasChildrenCurrent = contentEl.children().size() > 0;
+        if (!hasChildrenState && hasChildrenCurrent) {
             callback();
-            hasChildren = true;
         }
+        hasChildrenState = hasChildrenCurrent;
     });
 
-    observer.observe(contentEl.get(0), {subtree: true, attributes: true});
+    observer.observe(contentEl.get(0), {subtree: true, childList: true});
 };
-
-var css = `
-    .red {
-       color: red;
-    }
-
-    .nrcsubsection {
-        background-color: #fff;
-        height: 5px;
-    }
-
-    input.entryfield {
-        width: 70px;
-        height: 18px;
-        padding: 2px;
-    }
-
-    input.dataelementtotal {
-        width: 70px;
-        height: 16px;
-        padding: 2px;
-    }
-
-    input.indicator {
-        width: 70px;
-        height: 18px;
-        padding: 2px;
-    }
-
-    tr:hover {
-        background-color: #e5e5e5
-    }
-
-    th {
-        border-style: hidden !important;
-    }
-
-    td {
-        padding: 2px !important;
-        height: 16px;
-        text-align: center;
-        border-style: none !important;
-    }
-
-    td.nrcindicatorName {
-        text-align: left;
-    }
-
-    td.lastinrow {
-        background-color: #eee;
-        text-align: center;
-    }
-
-    th.nrcinfoheader {
-        text-align: left;
-        border-bottom-style: hidden;
-        border-left-style: hidden;
-        border-top-style: hidden;
-        width: 100%;
-        white-space: nowrap;
-        background-color: #fff;
-        padding: 2px !important;
-    }
-
-    th.nrcdataheader {
-        text-align: center;
-        border-bottom: 1px solid #ddd !important;
-        background-color: #eaf7fb;
-        white-space: nowrap;
-        padding-top: 2px !important;
-        padding-bottom: 2px !important;
-        padding-left: 10px;
-        padding-right: 10px;
-    }
-
-    th.nrctotalheader {
-        text-align: center;
-        border-bottom: 1px solid #ddd !important;
-        background-color: #eee;
-        white-space: nowrap;
-        padding-top: 2px !important;
-        padding-bottom: 2px !important;
-        padding-left: 10px;
-        padding-right: 10px;
-    }
-
-    .panel {
-        margin-bottom: 7px;
-    }
-
-    .panel-body {
-        padding-top: 0;
-        padding-left: 8px;
-        padding-right: 8px;
-        padding-bottom: 4px;
-    }
-
-    .panel-default > .panel-heading {
-        background-color: #3c3c3c;
-        border-color: #3c3c3c;
-        padding: 7px;
-        margin-bottom: 5px;
-        cursor: move;
-    }
-
-    a.nrc-panel-title {
-        color: white;
-        cursor: pointer;
-    }
-     
-    .accordion-toggle:after {
-        font-family: FontAwesome;
-        content: "\\f077";
-        float: right;
-        color: white !important;
-    }
-
-    .panel-heading.collapsed .accordion-toggle:after {
-        content: "\\f078";
-    }
-`;
 
 var loadCss = function(url) {
     $("<link/>", {
@@ -165,7 +47,9 @@ var injectCss = function(styles) {
 
 var emptyField = "__undefined";
 
-var getTag = function(el, separator, name) {
+var separator = "@";
+
+var getTag = function(el, name) {
     var idx = {section: 0, theme: 1, group: 2}[name];
     return $(el).text().split(separator)[idx] || emptyField;
 };
@@ -184,7 +68,7 @@ var getThemeHeader = function(title, target) {
     );
 };
 
-var processSection = function(tabsByTheme, sectionName) {
+var processGroupedTab = function(tabsByTheme, sectionName) {
     var groupedContents =
         $("<ul/>").addClass("list-unstyled").append(
             _.map(tabsByTheme, (tabsByGroup, themeName) => {
@@ -211,7 +95,7 @@ var processSection = function(tabsByTheme, sectionName) {
         );
 
     var tabs = _($("#tabs li").toArray())
-        .select(el => $(el).text() === sectionName || $(el).text().startsWith(sectionName + "@"));
+        .select(el => $(el).text().split(separator)[0] === sectionName);
     var mainTab = $(tabs[0]);
 
     getTabContents(mainTab).html(groupedContents);
@@ -219,14 +103,14 @@ var processSection = function(tabsByTheme, sectionName) {
     $(tabs.slice(1)).remove();
 };
 
-var getGroupedTabs = function(separator) {
+var getGroupedTabs = function() {
     return _.chain($("#tabs li").toArray())
-        .groupBy(el => getTag(el, separator, "section"))
+        .groupBy(el => getTag(el, "section"))
         .map((elementInSection, sectionName) => {
             var tabsByTheme = _.chain(elementInSection)
-                .groupBy(el => getTag(el, separator, "theme"))
+                .groupBy(el => getTag(el, "theme"))
                 .map((elementsInTheme, themeName) => {
-                    var tabsByGroup = _.groupBy(elementsInTheme, el => getTag(el, separator, "group"));
+                    var tabsByGroup = _.groupBy(elementsInTheme, el => getTag(el, "group"));
                     return [themeName, tabsByGroup];
                 })
                 .object()
@@ -237,21 +121,25 @@ var getGroupedTabs = function(separator) {
         .value();
 };
 
-var groupSubsections = function(options) {
-    var separator = options.separator || "@";
-    var groupedTabs = getGroupedTabs(separator);
-    _.each(groupedTabs, processSection);
+var groupSubsections = function() {
+    _.each(getGroupedTabs(), processGroupedTab);
 };
 
 var init = function() {
-    console.log("data-entry-contentscript: init");
-    runOnMutation($("#contentDiv"), () => {
-        groupSubsections({separator: "@"});
-    });
+    var contentDiv = $("#contentDiv");
+    var isDataEntryPage = dhis2.de.updateIndicators && contentDiv.length > 0;
 
-    loadJs("../dhis-web-commons/bootstrap/js/bootstrap.min.js");
-    loadCss("../dhis-web-commons/bootstrap/css/bootstrap.min.css");
-    injectCss(css);
+    if (isDataEntryPage) {
+        console.log("data-entry-contentscript: init");
+        runOnMutation(contentDiv, () => {
+            groupSubsections();
+        });
+
+        loadJs("../dhis-web-commons/bootstrap/js/bootstrap.min.js");
+        loadCss("../dhis-web-commons/bootstrap/css/bootstrap.min.css");
+    }
 };
 
 $(init);
+
+})();
