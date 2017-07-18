e04e89c89e10efdbb90597e5b717291b4d447dcf
Arnau Sanchez <pyarnau@gmail.com>
2017-07-18 13:48:53 +0200

    Add lodash mixins: cartesianProduct, groupConsecutive

--- src/utils/lodash-mixins.js
+++ src/utils/lodash-mixins.js
@@ -1,15 +1,41 @@
 import _ from 'lodash';
+import fp from 'lodash/fp';
 
 function deepMerge(object, source) {
-  return _.mergeWith(object, source, function(objValue, srcValue) {
-    if (_.isObject(objValue) && srcValue) {
-      return deepMerge(objValue, srcValue);
-    }
-  });
+    return _.mergeWith(object, source, function(objValue, srcValue) {
+        if (_.isObject(objValue) && srcValue) {
+            return deepMerge(objValue, srcValue);
+        }
+    });
 }
 
-_.mixin({
-		deepMerge: deepMerge,
-});
+function cartesianProduct(...rest) {
+    return fp.reduce((a, b) =>
+        fp.flatMap(x =>
+            fp.map(y => x.concat([y]))(b)
+        )(a)
+    )([[]])(rest);
+}
 
-export default _;
+function groupConsecutive(xs) {
+  return _(xs).reduce((acc, x) => {
+    if (_.isEmpty(acc)) {
+      return acc.concat([[x]]);
+    } else {
+      const last = _.last(acc);
+      if (_.last(last) === x) {
+        last.push(x);
+        return acc;
+      } else {
+        return acc.concat([[x]]);
+      }
+    }
+  },
+  [])
+}
+
+export {
+    deepMerge,
+    cartesianProduct,
+    groupConsecutive,
+};
