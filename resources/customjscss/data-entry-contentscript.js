3cb83792519e06f0add378daa7a1db61cc19e906
Arnau Sanchez <pyarnau@gmail.com>
2017-08-02 14:46:50 +0200

    Hide columns with greyed fields

--- resources/customjscss/data-entry-contentscript.js
+++ resources/customjscss/data-entry-contentscript.js
@@ -14,6 +14,48 @@
 
 (function() {
 
+var _ = window._.noConflict();
+
+_.mixin({
+    transpose: function(list) {
+        return _.zip.apply(_, list);
+    },
+
+    inGroupsOf: function(source, n) {
+        var dest = [];
+        while(source.length) {
+          dest.push(source.splice(0, n))
+        }
+        return dest;
+    },
+
+    cartesianProduct: function(args) {
+        return _.reduce(args, function(a, b) {
+            return _.flatten(_.map(a, function(x) {
+                return _.map(b, function(y) { return x.concat([y]); });
+            }), true);
+        }, [[]]);
+    },
+
+    groupConsecutiveBy: function(xs, mapper) {
+        mapper = mapper || _.identity;
+        var reducer = (acc, x) => {
+            if (_.isEmpty(acc)) {
+                return acc.concat([[x]]);
+            } else {
+                var last = _.last(acc);
+                if (_.isEqual(mapper(_.last(last)), mapper(x))) {
+                    last.push(x);
+                    return acc;
+                } else {
+                    return acc.concat([[x]]);
+                }
+            }
+        };
+        return _(xs).reduce(reducer, []);
+    },
+});
+
 var loadCss = function(url) {
     $("<link/>", {
         rel: "stylesheet",
@@ -106,6 +148,53 @@ var groupSubsections = function() {
     _.each(getGroupedTabs(), processGroupedTab);
 };
 
+var hideGreyedColumns = function() {
+    $(".sectionTable").not(".floatThead-table").get().map($).forEach(table => {
+        var nColumns = table.find("tbody tr:nth-child(1) td input").size();
+        var tdInputs = table.find("tbody tr td input").get();
+        var rows = _.chain(tdInputs).inGroupsOf(nColumns).value();
+        var columns = _.transpose(rows);
+        var disabledColumnIndexes = _.chain(columns)
+            .map((inputs, idx) => _.all(inputs, input => input.disabled) ? idx : null)
+            .reject(x => x === null)
+            .value();
+
+        var categoryOptionsList = _(table.find("thead tr"))
+            .map(tr => _.uniq(_.map($(tr).find("th"), th => $(th).text().trim())))
+        var allProducts =_.cartesianProduct(categoryOptionsList);
+        var products = _.chain(_.range(allProducts.length))
+            .difference(disabledColumnIndexes).map(idx => allProducts[idx]).value();
+        var categoryRows = _(_.range(categoryOptionsList.length)).map(categoryIndex => {
+            var groups = _(products).groupConsecutiveBy(xs => xs[categoryIndex]);
+            return _.map(groups, group => {
+                var label = group[0][categoryIndex];
+                var colspan = group.length;
+                return $("<th />").attr({colspan: colspan, scope: "col"})
+                    .append($("<span />", {align: "center"}).text(label))
+            });
+        });
+
+        // Replace headers
+        categoryRows.forEach((row, rowIndex) => {
+            var tr = table.find("thead tr:nth-child(" + (rowIndex + 1) + ")");
+            tr.find("th").remove()
+            tr.append(row);
+        });
+
+        // Hide disabled columns for values (it's an inplace operation, take indexes in reverse order)
+        _.chain(disabledColumnIndexes).reverse().each(disabledColumnIndex => {
+            // Offset nth-child by 1 (css counts start at 1) and 1 (skip dataElement name) = 2
+            var selector = "tbody tr td:nth-child(" + (disabledColumnIndex + 2) + ")";
+            table.find(selector).hide();
+        });
+    })
+};
+
+var applyChangesToForm = function() {
+    groupSubsections();
+    hideGreyedColumns();
+};
+
 var init = function() {
     var contentDiv = $("#contentDiv");
     var isDataEntryPage = window.dhis2 && window.dhis2.de &&
@@ -115,7 +204,7 @@ var init = function() {
     if (isDataEntryPage) {
         console.log("CustomJS: isDataEntryPage");
         
-        $(document).on( "dhis2.de.event.formLoaded", groupSubsections);
+        $(document).on( "dhis2.de.event.formLoaded", applyChangesToForm);
         loadJs("../dhis-web-commons/bootstrap/js/bootstrap.min.js");
         loadCss("../dhis-web-commons/bootstrap/css/bootstrap.min.css");
     }
