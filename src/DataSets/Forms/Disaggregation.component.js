1cebade26237462d16bbf9b4514e423188385aed
Arnau Sanchez <pyarnau@gmail.com>
2017-09-06 13:53:11 +0200

    Add search box for disaggregation step

--- src/DataSets/Forms/Disaggregation.component.js
+++ src/DataSets/Forms/Disaggregation.component.js
@@ -1,11 +1,30 @@
 import React, { PropTypes } from 'react';
 import Translate from 'd2-ui/lib/i18n/Translate.mixin';
+import ObserverRegistry from '../../utils/ObserverRegistry.mixin';
 import LinearProgress from 'material-ui/LinearProgress/LinearProgress';
 import DataSetElementCategoryComboSelectionDialog from '../../forms/DataSetElementCategoryComboSelectionDialog.component';
 import {getCategoryCombos, getCustomCategoryCombo} from '../../utils/Dhis2Helpers';
+import SearchBox from '../../SearchBox/SearchBox.component';
+
+const SearchBoxWrapper = (props) => {
+    const {debounce, onChange} = props;
+
+    const wrapperStyle = {
+        display: 'inline-block',
+        width: '50%',
+        position: 'relative',
+        margin: '5px 0px'
+    };
+
+    return (
+        <div style={wrapperStyle}>
+            <SearchBox searchObserverHandler={onChange} debounce={debounce || 50} />
+        </div>
+    );
+};
 
 const Disaggregation = React.createClass({
-    mixins: [Translate],
+    mixins: [Translate, ObserverRegistry],
 
     propTypes: {
         validateOnRender: React.PropTypes.bool,
@@ -14,12 +33,15 @@ const Disaggregation = React.createClass({
     getInitialState() {
         return {
             categoryCombos: null,
+            filter: "",
         };
     },
 
     componentDidMount() {
         getCategoryCombos(this.context.d2).then(categoryCombos => {
-            this.setState({categoryCombos: categoryCombos.toArray().filter(cc => !cc.isDefault)});
+            this.setState({
+                categoryCombos: categoryCombos.toArray().filter(cc => !cc.isDefault),
+            });
         });
     },
 
@@ -37,14 +59,23 @@ const Disaggregation = React.createClass({
         this.forceUpdate();
     },
 
+    _onSearchChange(searchObserver) {
+        this.registerDisposable(searchObserver.subscribe(s => this.setState({filter: s})));
+    },
+
     _renderForm() {
         const {dataSetElements} = this.props.store.dataset;
-        const {categoryCombos} = this.state;
+        const {categoryCombos, filter} = this.state;
+        const isSubstring = (s1, s2) => _.includes(s1.toLowerCase(), s2.toLowerCase());
+        const filteredDataSetElements = dataSetElements
+            .filter(dse => !filter || isSubstring(dse.dataElement.displayName, filter));
 
         return (
             <div>
+                <SearchBoxWrapper onChange={this._onSearchChange} />
+
                 <DataSetElementCategoryComboSelectionDialog
-                    dataSetElements={dataSetElements}
+                    dataSetElements={filteredDataSetElements}
                     categoryCombos={categoryCombos}
                     onCategoryComboSelected={this._onCategoryComboSelected}
                     d2={this.context.d2}
