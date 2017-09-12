b735507cb4c6bc362ca7b08b1e5941bcba1085bb
Arnau Sanchez <pyarnau@gmail.com>
2017-09-12 16:47:14 +0200

    [dataentry] Show group/de-name only for the first split table

--- resources/customjscss/data-entry-contentscript.js
+++ resources/customjscss/data-entry-contentscript.js
@@ -205,27 +205,32 @@ var hideGreyedColumnsAndSplit = function() {
             showRowTotals: table.find("tbody tr:first-child td:last-child input.total-cell").size() > 0,
             showColumnTotals: table.find("tbody tr:last-child td:nth-child(2) input.total-cell").size() > 0,
         };
-        var newTables = splitTables(data, 0);
+
+        var newTables = splitTables(data, {categoryIndex: 0, tableIndex: 0});
         table.replaceWith($("<div>").append(newTables));
     });
 };
 
-var splitTables = function(data, index) {
+var splitTables = function(data, options) {
+    var categoryIndex = options.categoryIndex;
     var nCategories = data.categories.length;
-    var table = buildTable(data);
+    var renderDataElementInfo = (options.tableIndex === 0);
+    var table = buildTable(data, renderDataElementInfo);
 
-    if (index >= nCategories - 1 || tableFitsInViewport(table)) {
+    if (categoryIndex >= nCategories - 1 || tableFitsInViewport(table)) {
         return [table];
     } else {
         return _.chain(data.cocs)
-            .groupConsecutiveBy(coc => coc.cos.slice(0, index + 1))
-            .map(splitCocs => splitTables(_.extend({}, data, {cocs: splitCocs}), index + 1))
+            .groupConsecutiveBy(coc => coc.cos.slice(0, categoryIndex + 1))
+            .map((splitCocs, splitTableIndex) =>
+                splitTables(_.extend({}, data, {cocs: splitCocs}),
+                    {categoryIndex: categoryIndex + 1, tableIndex: options.tableIndex + splitTableIndex}))
             .flatten(1)
             .value();
     }
 };
 
-var buildTable = function(data) {
+var buildTable = function(data, renderDataElementInfo) {
     var getValues = (row) => data.cocs.map(coc => row.valuesByCocId[coc.id]);
     var nCategories = data.categories.length;
     var categoryThsList = _.range(nCategories).map(categoryIndex => {
@@ -241,7 +246,8 @@ var buildTable = function(data) {
     return $("<table>", {id: "sectionTable", class: "sectionTable", cellspacing: "0"}).append([
         $("<thead>").append(
             categoryThsList.map((categoryThs, index) => $("<tr>").append(
-                $("<th>", {class: "nrcinfoheader"}).html(index === 0 && data.group || "&nbsp;"),
+                $("<th>", {class: "nrcinfoheader"})
+                    .html(renderDataElementInfo && index === 0 ? data.group : "&nbsp;"),
                 categoryThs,
                 index === 0 && data.showRowTotals ?
                     $("<th>", {class: "nrctotalheader", rowspan: nCategories, verticalAlign: "top"}).text("Total") :
@@ -254,7 +260,8 @@ var buildTable = function(data) {
                 var rowTotalId = ["row", row.de.id].concat(getValues(row).map(val => val.coc)).join("-");
                 var rowTotal = $("<input>", {class: "dataelementtotal", type: "text", disabled: "", id: rowTotalId});
                 return $("<tr>").append(
-                    $("<td>", {class: "nrcindicatorName"}).text(row.de.name),
+                    $("<td>", {class: "nrcindicatorName"})
+                        .html(renderDataElementInfo ? row.de.name : "&nbsp;"),
                     getValues(row).map(val => val.td.clone()),
                     data.showRowTotals ? $("<td>").append(rowTotal) : null
                 );
