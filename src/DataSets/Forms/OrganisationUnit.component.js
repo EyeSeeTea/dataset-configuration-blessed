3c7e334efca9194b5a19fdde055b1291c11543ca
Arnau Sanchez <pyarnau@gmail.com>
2017-12-21 11:20:12 +0100

    Show error message on orgUnit step if no units selected

--- src/DataSets/Forms/OrganisationUnit.component.js
+++ src/DataSets/Forms/OrganisationUnit.component.js
@@ -5,11 +5,12 @@ import LinearProgress from 'material-ui/LinearProgress/LinearProgress';
 import Card from 'material-ui/Card/Card';
 import CardText from 'material-ui/Card/CardText';
 import OrganisationUnitTreeMultiSelect from 'd2-ui/lib/org-unit-select/orgunit-tree-multi-select';
-
 import OrgUnitTree from 'd2-ui/lib/org-unit-tree/OrgUnitTree.component';
 import OrgUnitSelectByLevel from 'd2-ui/lib/org-unit-select/OrgUnitSelectByLevel.component';
 import OrgUnitSelectByGroup from 'd2-ui/lib/org-unit-select/OrgUnitSelectByGroup.component';
 import OrgUnitSelectAll from 'd2-ui/lib/org-unit-select/OrgUnitSelectAll.component';
+import scrollToComponent from 'react-scroll-to-component';
+import {collectionToArray} from '../../utils/Dhis2Helpers';
 
 const OrganisationUnit = React.createClass({
     mixins: [Translate],
@@ -20,11 +21,19 @@ const OrganisationUnit = React.createClass({
     },
 
     getInitialState() {
-        return {};
+        return {errors: null};
     },
 
     componentWillReceiveProps(props) {
-        props.formStatus(true);
+        if (props.validateOnRender) {
+            const organisationUnits = collectionToArray(this.props.store.dataset.organisationUnits);
+            if (_(organisationUnits).isEmpty()) {
+                this.setState({errors: this.getTranslation('select_one_organisation_unit')});
+                scrollToComponent(this.refs.errors);
+            } else {
+                props.formStatus(true);
+            }
+        }
     },
 
     _renderSharingWarning() {
@@ -32,6 +41,10 @@ const OrganisationUnit = React.createClass({
         if (!project && !_.isEmpty(countries)) {
             return (<p>{this.getTranslation('sharing_warning')}</p>);
         }
+    },
+
+    _renderErrors() {
+        return <p ref="errors" style={{color: 'red'}}>{this.state.errors}</p>;
     },
 
     _onChange(orgUnits) {
@@ -45,6 +58,7 @@ const OrganisationUnit = React.createClass({
         return (
             <div>
                 {this._renderSharingWarning()}
+                {this._renderErrors()}
                 <OrganisationUnitTreeMultiSelect
                     modelDefinition={modelDefinition}
                     model={model}
