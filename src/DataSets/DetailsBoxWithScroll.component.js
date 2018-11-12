a0cd8e7764de0c3dd980de44d413d760e8f46f3e
Arnau Sanchez <pyarnau@gmail.com>
2018-11-12 10:25:31 +0100

    Fix DetailsBoxWithScroll scroll follow

--- src/DataSets/DetailsBoxWithScroll.component.js
+++ src/DataSets/DetailsBoxWithScroll.component.js
@@ -4,12 +4,10 @@ import DetailsBox from './DetailsBox.component';
 import Paper from 'material-ui/Paper';
 
 export default class DetailsBoxWithScroll extends Component {
-
     componentDidMount() {
         this.disposable = Observable
             .fromEvent(global, 'scroll')
             .debounce(200)
-            .map(() => document.querySelector('body').scrollTop)
             .subscribe(() => this.forceUpdate());
     }
 
@@ -18,9 +16,12 @@ export default class DetailsBoxWithScroll extends Component {
     }
 
     render() {
+        const appOffsetTop = document.querySelector('main').offsetTop;
+        const marginTop = Math.max(document.scrollingElement.scrollTop - appOffsetTop, 0);
+
         return (
             <div style={this.props.style}>
-                <Paper zDepth={1} rounded={false} style={{ maxWidth: 500, minWidth: 300, marginTop: document.querySelector('body').scrollTop }}>
+                <Paper zDepth={1} rounded={false} style={{ maxWidth: 500, minWidth: 300, marginTop }}>
                     <DetailsBox
                         source={this.props.detailsObject}
                         showDetailBox={!!this.props.detailsObject}
