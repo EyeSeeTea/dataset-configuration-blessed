6af93d1958cfb0e1db06381a74d7b7074834a693
Arnau Sanchez <pyarnau@gmail.com>
2019-11-27 12:09:29 +0100

    Show also data sharing in listing

--- src/MultipleDataTable/MultipleDataTableRow.component.js
+++ src/MultipleDataTable/MultipleDataTableRow.component.js
@@ -94,19 +94,7 @@ const MultipleDataTableRow = React.createClass({
 
             // TODO: PublicAccess Hack - need to make it so that value transformers can be registered
             if (columnName === "publicAccess") {
-                if (dataSource[columnName]) {
-                    if (dataSource[columnName] === "rw------") {
-                        displayValue = this.getTranslation("public_can_edit");
-                    }
-
-                    if (dataSource[columnName] === "r-------") {
-                        displayValue = this.getTranslation("public_can_view");
-                    }
-
-                    if (dataSource[columnName] === "--------") {
-                        displayValue = this.getTranslation("public_none");
-                    }
-                }
+                displayValue = this.getDataMetadaAccessText(dataSource[columnName]);
             }
 
             return (
@@ -155,6 +143,29 @@ const MultipleDataTableRow = React.createClass({
 
     handleClick(event) {
         this.props.primaryClick(event, this.props.dataSource);
+    },
+
+    getDataMetadaAccessText(value) {
+        if (value) {
+            const accessMetadata = value.slice(0, 2);
+            const accessData = value.slice(2, 4);
+            const get = this.getAccessText.bind(this);
+            return `Data: ${get(accessMetadata)}, Metadata: ${get(accessData)}`;
+        } else {
+            return "?";
+        }
+    },
+
+    getAccessText(value) {
+        if (!value) {
+            return "?";
+        } else if (value === "rw") {
+            return this.getTranslation("public_can_edit");
+        } else if (value === "r-") {
+            return this.getTranslation("public_can_view");
+        } else if (value === "--") {
+            return this.getTranslation("public_none");
+        }
     },
 });
 
